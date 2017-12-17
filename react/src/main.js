53af8b535939ffc77883579a3b0ca8d2f14b2ca5
Mark Davis <markdavis.070@gmail.com>
2017-12-17 10:27:06 -0500

    add react and codeship

---
+++ react/src/main.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from "./App"
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h1>This is the begining.....</h1>,
+    document.getElementById('app')
+  );
+})
